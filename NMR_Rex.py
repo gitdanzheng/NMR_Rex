36ecf2501776020bc0dabe46ecbfb835cd17dd76
dan zheng <danz@email.com>
2020-06-27 23:52:03 +1000

    formal import

--- NMR_Rex.py
+++ NMR_Rex.py
@@ -1,13 +1,9 @@
-
-from PyQt5.QtWidgets import QFileDialog
-from PyQt5.QtWidgets import QWidget, QApplication
-from pandas import DataFrame as pddf
-from pandas import read_csv
-from matplotlib.pyplot import subplots
-from glob import glob
+from PyQt5.QtWidgets import QWidget, QApplication, QFileDialog
+import pandas as pd
+import matplotlib.pyplot as plt
+import glob
 import re
-from os import chdir
-
+import os
 import sys
 
 from rex_ui import Ui_Form  # pyuic5 generated code
@@ -39,19 +35,19 @@ class MainWindows(QWidget, Ui_Form):
 
     def run(self):
         folder = self.lineEdit.text()
-        df = pddf(columns=["residue_number", "residue", "Rex"])
+        df = pd.DataFrame(columns=["residue_number", "residue", "Rex"])
         i = 0
         try:
-            chdir(folder)
+            os.chdir(folder)
 
-            file_list = glob("*.cpmg")   # get a list of *.cpmg files
+            file_list = glob.glob("*.cpmg")   # get a list of *.cpmg files
             for file in file_list:
                 with open(file):
                     name = file[0]
                     # grab unumber from file name as residue no.
                     residue_no = int(re.findall(r"\d+", file)[0])
                     # columns separated by one or more spaces
-                    data = read_csv(file, header=0, sep=r"\s+")
+                    data = pd.read_csv(file, header=0, sep=r"\s+")
                     max_index = data["#nu_cpmg(Hz)"].idxmax()  # index of maximum frequency
                     min_index = data["#nu_cpmg(Hz)"].idxmin()
                     R2_max_f = data.loc[max_index]["R2(1/s)"]  # R2 of maximum frequency
@@ -63,12 +59,12 @@ class MainWindows(QWidget, Ui_Form):
             df = df.sort_values("residue_number")
             filt = (df["residue_number"] < 1000)
 
-            fig, ax = subplots()
+            fig, ax = plt.subplots()
             df.loc[filt].plot(x="residue_number", y="Rex", legend=False, ax=ax)
             ax.set_xlabel("residue number")
             ax.set_ylabel("Rex")
             fig.tight_layout()
-            fig.show()
+            plt.show()
             # how to show plot in label?
             # plt.savefig("Rex.png")
         except OSError:
